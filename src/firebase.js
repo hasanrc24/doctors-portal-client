5b89de1532002444ee231f4cd138fe5ee3e4674b
hasanrc24 <85833993+hasanrc24@users.noreply.github.com>
2022-03-17 22:36:04 +0600

    Last commit

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAXX22hTexPhtOAZTY2M6cevdfmJx_kDg8",
-    authDomain: "doctors-portal-36927.firebaseapp.com",
-    projectId: "doctors-portal-36927",
-    storageBucket: "doctors-portal-36927.appspot.com",
-    messagingSenderId: "133599813770",
-    appId: "1:133599813770:web:42994bb64f63d8edc88d73",
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
   };
    const app = initializeApp(firebaseConfig)
  export default app;
